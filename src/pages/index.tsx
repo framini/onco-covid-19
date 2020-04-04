b3e6710165e7db66ead6051fc3650683a1e66941
Francisco Ramini <framini@gmail.com>
2020-04-04 17:06:55 -0300

    add contentful, now and preact setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
+import { NextPageContext } from 'next';
 import { Button } from '@chakra-ui/core';
+import { Article } from '../types';
 
-const Home = () => (
-  <div className="container">
-    <Button>Something</Button>
-  </div>
-);
+interface HomeProps {
+  articles: Article[];
+}
+
+const Home = (props: HomeProps) => {
+  console.log('props.articles', props.articles);
+
+  return (
+    <div className="container">
+      <Button>Something</Button>
+      <ul>
+        {props.articles.map((article: Article) => {
+          return <div>{article.fields.title}</div>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export async function getStaticProps(context: NextPageContext) {
+  const { client } = require('../contentful/client');
+
+  const entries = await client.getEntries({
+    content_type: 'articulo',
+  });
+
+  return {
+    props: {
+      articles: entries.items,
+    },
+  };
+}
 
 export default Home;
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Article {
+  fields: {
+    title: string;
+  };
+}
