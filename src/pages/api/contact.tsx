d908025872518ac23530fd08dd42037e52d9d615
Francisco Ramini <framini@gmail.com>
2020-04-25 18:24:32 -0300

    remove loggers

--- src/pages/api/contact.tsx
+++ src/pages/api/contact.tsx
@@ -6,15 +6,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const token = req.body.token;
   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${token}`;
 
-  console.log('url', url);
-  console.log('token', token);
-
   try {
     const googleResponse = await fetch(url, {
       method: 'post',
     }).then((response) => response.json());
-
-    console.log('googleResponse', googleResponse);
 
     // @ts-ignore
     if (googleResponse.success && googleResponse.score > 0.1) {
@@ -23,8 +18,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(400).json({ ok: false });
     }
   } catch (error) {
-    console.log('ERROR', error.message);
-
     const { response } = error;
     return response
       ? res.status(response.status).json({ message: response.statusText })
