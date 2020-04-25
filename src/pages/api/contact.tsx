c6af90a8d2f4980a2d7748c275b573bee545e7d4
Francisco Ramini <framini@gmail.com>
2020-04-25 18:04:17 -0300

    add some loggers on the api

--- src/pages/api/contact.tsx
+++ src/pages/api/contact.tsx
@@ -6,18 +6,25 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const token = req.body.token;
   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${token}`;
 
+  console.log('url', url);
+  console.log('token', token);
+
   try {
     const googleResponse = await fetch(url, {
       method: 'post',
     }).then((response) => response.json());
 
+    console.log('googleResponse', googleResponse);
+
     // @ts-ignore
-    if (googleResponse.success) {
+    if (googleResponse.success && googleResponse.score > 0.1) {
       return res.status(200).json({ ok: true });
     } else {
       return res.status(400).json({ ok: false });
     }
   } catch (error) {
+    console.log('ERROR', error.message);
+
     const { response } = error;
     return response
       ? res.status(response.status).json({ message: response.statusText })
