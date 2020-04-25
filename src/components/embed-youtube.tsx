96a343570e4c82fd923c62753fe5fcbd03e71f7b
Francisco Ramini <framini@gmail.com>
2020-04-25 11:22:42 -0300

    improve youtube embed loading, add favicon and related files

--- src/components/embed-youtube.tsx
+++ src/components/embed-youtube.tsx
@@ -1,55 +1,134 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { CircularProgress, Box, useTheme } from '@chakra-ui/core';
+import { FaPlayCircle } from 'react-icons/fa';
+import { LazyEmbed } from '@framini/react-lazy-embed';
+
+interface EmbedOverlayProps {
+  children: ReactNode;
+}
+
+const EmbedOverlay = (props: EmbedOverlayProps) => {
+  return (
+    <Box
+      position="absolute"
+      top={0}
+      left={0}
+      width="100%"
+      height="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="rgba(33, 150, 243, 0.25)"
+    >
+      {props.children}
+    </Box>
+  );
+};
+
+const getVideoId = (tentativeUrl: string): string => {
+  try {
+    const url = new URL(tentativeUrl);
+
+    let id;
+    // @ts-ignore
+    for (const [key, value] of url.searchParams.entries()) {
+      if (key === 'v') {
+        id = value;
+      }
+    }
+
+    return id;
+  } catch (e) {
+    return '';
+  }
+};
 
 interface EmbedYouTubeProps {
   uri: string;
 }
 
 export const EmbedYouTube = ({ uri }: EmbedYouTubeProps) => {
-  const [id, setId] = React.useState(null);
-
-  React.useEffect(() => {
-    try {
-      const url = new URL(uri);
-
-      let id;
-      // @ts-ignore
-      for (const [key, value] of url.searchParams.entries()) {
-        if (key === 'v') {
-          id = value;
-        }
-      }
-
-      if (id) {
-        setId(id);
-      }
-    } catch (e) {}
-  }, [uri]);
+  const id = getVideoId(uri);
+  const theme = useTheme();
 
   if (!id) {
     return null;
   }
 
   return (
-    <div
-      className="video"
-      style={{
-        position: 'relative',
-        paddingBottom: '56.25%' /* 16:9 */,
-        paddingTop: 25,
-        height: 0,
+    <LazyEmbed id={id}>
+      {({ status, onActivate, iframe, thumbnail }) => {
+        const showThumb = status === 'visible' || status === 'load';
+        const showIframe = status === 'load' || status === 'loaded';
+
+        return (
+          <Box
+            borderRadius="4px"
+            overflow="hidden"
+            backgroundColor="#000"
+            style={
+              showThumb
+                ? {
+                    position: 'relative',
+                    overflow: 'hidden',
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }
+                : {}
+            }
+          >
+            {showThumb && (
+              <>
+                <picture>
+                  <source srcSet={thumbnail.webp} type="image/webp" />
+                  <img src={thumbnail.jpg} />
+                </picture>
+
+                <EmbedOverlay>
+                  {status === 'load' ? (
+                    <CircularProgress
+                      isIndeterminate
+                      color="blue"
+                    ></CircularProgress>
+                  ) : (
+                    <button onClick={onActivate}>
+                      <FaPlayCircle
+                        style={{
+                          width: 60,
+                          height: 60,
+                          color: theme.colors.white,
+                        }}
+                      />
+                    </button>
+                  )}
+                </EmbedOverlay>
+              </>
+            )}
+
+            {showIframe && (
+              <div
+                style={{
+                  position: 'relative',
+                  paddingBottom: '56.25%' /* 16:9 */,
+                  paddingTop: 25,
+                  height: 0,
+                }}
+              >
+                <iframe
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                  }}
+                  {...iframe}
+                />
+              </div>
+            )}
+          </Box>
+        );
       }}
-    >
-      <iframe
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        }}
-        src={`https://www.youtube.com/embed/${id}`}
-        frameBorder="0"
-      />
-    </div>
+    </LazyEmbed>
   );
 };
