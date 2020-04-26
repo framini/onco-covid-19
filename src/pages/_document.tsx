4cbf811d476cb6103393bdfc3695ac8c56077e5a
Francisco Ramini <framini@gmail.com>
2020-04-26 12:30:56 -0300

    test for self-hosting fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,8 +17,39 @@ export default class OncoCovid19Document extends Document<any> {
             dangerouslySetInnerHTML={{ __html: this.props.css }}
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&family=Open+Sans:wght@400;600&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+            href="/fonts/open-sans-v17-latin-600.woff2"
+          />
+          <link
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+            href="/fonts/open-sans-v17-latin-regular.woff2"
+          />
+          <link
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+            href="/fonts/pt-serif-v11-latin-700.woff2"
+          ></link>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              try{new FontFace('Open Sans__subset',"url(/fonts/open-sans-v17-latin-600.woff2) format('woff2'), url(/subfont/Open_Sans-600-2af9648f10.woff) format('woff')",{'font-weight':600}).load(),new FontFace('Open Sans__subset',"url(/fonts/open-sans-v17-latin-regular.woff2) format('woff2'), url(/subfont/Open_Sans-400-abaefcd05c.woff) format('woff')",{}).load(),new FontFace('PT Serif__subset',"url(/fonts/pt-serif-v11-latin-700.woff2) format('woff2'), url(/subfont/PT_Serif-700-c2db6364b1.woff) format('woff')",{'font-weight':700}).load()}catch(e){}
+            `,
+            }}
+          ></script>
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+              @font-face{font-display:swap;font-family:'Open Sans';font-style:normal;font-weight:400;src:local('Open Sans Regular'),local('OpenSans-Regular'),url(/fonts/open-sans-v17-latin-regular.woff2) format('woff2'),url(/fonts/open-sans-v17-latin-regular.woff) format('woff')}@font-face{font-display:swap;font-family:'Open Sans';font-style:normal;font-weight:600;src:local('Open Sans SemiBold'),local('OpenSans-SemiBold'),url(/fonts/open-sans-v17-latin-600.woff2) format('woff2'),url(/fonts/open-sans-v17-latin-600.woff) format('woff')}@font-face{font-display:swap;font-family:'PT Serif';font-style:normal;font-weight:700;src:local('PT Serif Bold'),local('PTSerif-Bold'),url(/fonts/pt-serif-v11-latin-700.woff2) format('woff2'),url(/fonts/pt-serif-v11-latin-700.woff) format('woff')}
+              `,
+            }}
           />
           <link
             rel="apple-touch-icon"
